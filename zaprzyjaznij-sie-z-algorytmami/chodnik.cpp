d11894c8d78b191d83dceeceb1a7d5f47ae20624
Martyna Woźniak <133137213+martynawozniak@users.noreply.github.com>
2024-09-05 23:37:12 +0200

    Update chodnik.cpp

--- zaprzyjaznij-sie-z-algorytmami/chodnik.cpp
+++ zaprzyjaznij-sie-z-algorytmami/chodnik.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main () {
-    long long n, l, suma;
+    long long n, l, sum;
     cin >> n;
-    suma = n*(n+1)/2;
+    sum = n*(n+1)/2;
     for(int i = 0; i < n-1; i++) {
         cin >> l;
-        suma -= l;
+        sum -= l;
     }
-    cout << suma;
+    cout << sum;
     return 0;
 }
